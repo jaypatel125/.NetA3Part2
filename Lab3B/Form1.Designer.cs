8f93fbba9d777b7704304daf329e7b76061d3c9e
sujan-22 <sujanrokad44@gmail.com>
2023-10-25 16:19:16 -0400

    Add project files.

---
+++ Lab3B/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Lab3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(35, 35);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(160, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "select a hairdresser:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listBox1);
+            this.groupBox2.Location = new System.Drawing.Point(224, 35);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 154);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "select a service:";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox2);
+            this.groupBox3.Location = new System.Drawing.Point(455, 35);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 154);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "charged items:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.listBox3);
+            this.groupBox4.Location = new System.Drawing.Point(687, 35);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(154, 154);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "price:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Cut",
+            "Wash, blow-dry, and style",
+            "Clour",
+            "Highlights",
+            "Extensions",
+            "Up-do"});
+            this.listBox1.Location = new System.Drawing.Point(6, 21);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(188, 100);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(6, 21);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(188, 100);
+            this.listBox2.TabIndex = 4;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(7, 22);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 84);
+            this.listBox3.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(660, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Total Price:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(741, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 273);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 41);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Add Service";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 273);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 41);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Calculate Total Price";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(444, 273);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 41);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(608, 273);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 41);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Jane",
+            "Pat",
+            "Ron",
+            "Sue",
+            "Laurie"});
+            this.comboBox1.Location = new System.Drawing.Point(7, 21);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Jane";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 339);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "g";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Lab3B/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3B
+{
+    public partial class Form1 : Form
+    {
+        private bool hairdresserAdded = false;
+        private int totalPrice = 0;
+        int hairDresserPrice;
+        List<int> total = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Check if a hairdresser is selected
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select a hairdresser.");
+                return;
+            }
+
+            
+
+            // Get the selected hairdresser
+            string selectedHairdresser = comboBox1.SelectedItem.ToString();
+
+            // Get the selected services
+            List<string> selectedServices = new List<string>();
+            foreach (var selectedItem in listBox1.SelectedItems)
+            {
+                selectedServices.Add(selectedItem.ToString());
+            }
+
+            
+            if(comboBox1.SelectedIndex == 0) {
+                comboBox1.Enabled = false;
+                hairDresserPrice = 30;
+
+            } else if(comboBox1.SelectedIndex == 1)
+            {
+                comboBox1.Enabled = false;
+                hairDresserPrice = 45;
+            } else if (comboBox1.SelectedIndex == 2)
+            {comboBox1.Enabled = false;
+                hairDresserPrice = 40;
+            } else if (comboBox1.SelectedIndex == 3)
+            {comboBox1.Enabled = false;
+                hairDresserPrice = 50;
+            } else
+            {comboBox1.Enabled = false;
+                hairDresserPrice = 55;
+            }
+
+            // Add the selected hairdresser if it hasn't been added before
+            if (!hairdresserAdded)
+            {
+                listBox2.Items.Add($"{comboBox1.SelectedItem.ToString()}");
+                listBox3.Items.Add($"${hairDresserPrice}");
+                hairdresserAdded = true;
+            }
+
+
+            int servicePrice = 0;
+            
+
+            foreach (var selectedItemIndex in listBox1.SelectedIndices)
+            {
+                // Use the index to calculate the service price
+                switch (selectedItemIndex)
+                {
+                    case 0:
+                        servicePrice = 30;
+                        total.Add(servicePrice);
+                        break;
+                    case 1:
+                        servicePrice = 20;
+                        total.Add(servicePrice);
+                        break;
+                    case 2:
+                        servicePrice = 40;
+                        total.Add(servicePrice);
+                        break;
+                    case 3:
+                        servicePrice = 50;
+                        total.Add(servicePrice);
+                        break;
+                    case 4:
+                        servicePrice = 200;
+                        total.Add(servicePrice);
+                        break;
+                    case 5:
+                        servicePrice = 60;
+                        total.Add(servicePrice);
+                        break;
+                }
+                Console.WriteLine(servicePrice);
+            }
+            
+
+
+            // Display the selected hairdresser, services, and total price in the ListBox
+            
+            foreach (var service in selectedServices)
+            {
+                listBox2.Items.Add(service);
+                listBox2.Enabled = false;
+                listBox3.Enabled = false;
+            }
+
+
+
+            //double totalPrice;
+            listBox3.Items.Add($"${servicePrice}");
+            
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            foreach (int i in total)
+            {
+                count += i;
+                Console.WriteLine(count);
+            }
+
+            totalPrice = hairDresserPrice + count;
+            textBox1.Text = "$"+totalPrice.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            // Check if at least one service is selected
+            if (listBox1.SelectedIndex != -1)
+            {
+                button1.Enabled=true;
+                button2.Enabled=true;
+                return;
+            } 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+        }
+    }
+}
